68f8a35da82e045891c8d6163208c1b5909d7269
Dean Ludwig <49414433+amosdean@users.noreply.github.com>
2026-03-22 16:18:29 -0400

    button binary count init

---
+++ binary-count-button/main.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+// #define BUTTON (1 << PB0)
+
+int main(void)
+{
+    DDRD |= (1 << PD7);
+    DDRD &= ~(1 << PD5); // button input
+    PORTD |= (1 << PD5); // enable pull-up
+    while(1) {
+        if (!(PIND & (1 << PD5))) {
+            PORTD ^= (1 << PD7);   // LED ON
+            _delay_ms(10);
+        }
+    }
+}
+    // DDRD = 0xFF;              // LEDs on PORTD
+    // DDRB &= ~(1 << PB0);      // button input
+    // PORTB |= (1 << PB0);      // enable pull-up
+
+    // uint8_t counter = 0;
+    // // printf("DDRD: %02X\n", DDRD);
+    // // printf("DDRB: %02X\n", DDRB);
+    // // printf("PORTB: %02X\n", PORTB);
+    // while (1)
+    // {
+    //     if (!(PINB & BUTTON))
+    //     {
+    //         counter++;
+    //         PORTD = counter;
+    //         _delay_ms(200);
+    //     }
+    // }
--- toggle-button/main.c
+++ toggle-button/main.c
@@ -8,27 +8,12 @@ int main(void)
     DDRD |= (1 << PD7);
     DDRD &= ~(1 << PD5); // button input
     PORTD |= (1 << PD5); // enable pull-up
+    int num = 0;
     while(1) {
-        if (!(PIND & (1 << PD5))) {
-            PORTD ^= (1 << PD7);   // LED ON
-            _delay_ms(100);
-        }
+        // if (!(PIND & (1 << PD5))) {
+        //     PORTD ^= (1 << PD7);   // LED ON
+        //     _delay_ms(10);
+        // }
+        // case num:
     }
 }
-    // DDRD = 0xFF;              // LEDs on PORTD
-    // DDRB &= ~(1 << PB0);      // button input
-    // PORTB |= (1 << PB0);      // enable pull-up
-
-    // uint8_t counter = 0;
-    // // printf("DDRD: %02X\n", DDRD);
-    // // printf("DDRB: %02X\n", DDRB);
-    // // printf("PORTB: %02X\n", PORTB);
-    // while (1)
-    // {
-    //     if (!(PINB & BUTTON))
-    //     {
-    //         counter++;
-    //         PORTD = counter;
-    //         _delay_ms(200);
-    //     }
-    // }
