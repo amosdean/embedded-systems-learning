0ead727808b8b4a39e9a0684611f227f1ba69f5a
Dean Ludwig <49414433+amosdean@users.noreply.github.com>
2026-08-11 07:42:54 -0400

    starting refactoring motor files

--- STM32/stm32-car/Core/Src/motor.c
+++ STM32/stm32-car/Core/Src/motor.c
@@ -4,7 +4,16 @@ static Motor motors[4];
 static uint8_t latch_state;
 static const uint8_t DEFAULT_FREQ = 125;
 
-stati void latch_tx(void) {
-    // latch low
-    
+static void latch_tx(void) {
+    MOTORLATCH_LOW;
+    MOTORDATA_LOW;
+    for(int i = 7; i >= 0; i--) {
+        MOTORCLK_LOW;
+        if(latch_state & (1 << i))
+            MOTORDATA_HIGH;
+        else
+            MOTORDATA_LOW;
+        MOTORCLK_HIGH;
+    }
+    MOTORLATCH_HIGH;
 }
