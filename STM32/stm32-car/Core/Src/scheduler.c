5c986d63be273d54b68138da8c02be8ccb0df33c
Dean Ludwig <49414433+amosdean@users.noreply.github.com>
2026-08-17 07:26:21 -0400

    Scheduler bugs

---
+++ STM32/stm32-car/Core/Src/scheduler.c
@@ -0,0 +1,30 @@
+#include "scheduler.h"
+#include "motor.h"
+
+static void executeManeuver(const Maneuver maneuver) {
+    setPWM(FRONT_LEFT, maneuver.leftSpeed);
+    setPWM(FRONT_RIGHT, maneuver.rightSpeed);
+    run(FRONT_LEFT, maneuver.leftCmd);
+    run(FRONT_RIGHT, maneuver.rightCmd);
+}
+
+void schedulerStart(Scheduler scheduler) {
+    executeManeuver(scheduler.sequence[scheduler.current_maneuver]);
+}
+void tickScheduler(Scheduler *scheduler) {
+    const Maneuver *cur_maneuver = &scheduler->sequence[scheduler->current_maneuver];
+    // cur_maneuver++;
+    scheduler->elapsedTime++;
+
+    if (scheduler->elapsedTime >= scheduler->sequence[scheduler->current_maneuver].duration) {
+        // move to next maneuver
+        scheduler->current_maneuver++;
+        scheduler->elapsedTime = 0;
+
+        // loop maneuver sequence
+        if (scheduler->current_maneuver >= scheduler->length) 
+            scheduler->current_maneuver = 0;
+        
+        executeManeuver(*cur_maneuver);
+    }
+}
---
+++ STM32/stm32-car/Core/Src/scheduler.h
@@ -0,0 +1,24 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "stm32c0xx.h"
+
+typedef struct {
+    uint8_t leftCmd;
+    uint8_t rightCmd;
+    uint8_t leftSpeed;
+    uint8_t rightSpeed;
+    uint8_t duration;
+} Maneuver;
+
+typedef struct {
+    const Maneuver *sequence;
+    uint8_t length;
+    uint8_t current_maneuver;
+    uint8_t elapsedTime;
+} Scheduler;
+
+void initScheduler(Scheduler *scheduler);
+void tickScheduler(Scheduler *scheduler);
+
+#endif
